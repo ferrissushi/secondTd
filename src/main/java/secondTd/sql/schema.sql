eb04d47b7029baf2ea4a5965e87ab52f2a7f943d
NickyHariniaina <nickyharini1na@gmail.com>
2025-12-17 13:48:50 +0300

    chore: cerate all sql files

---
+++ src/main/java/secondTd/sql/schema.sql
@@ -0,0 +1,20 @@
+create type dish_types as enum ('START', 'MAIN', 'DESSERT');
+
+create table if not exists dish(
+    id serial primary key,
+    name varchar not null,
+    dish_type dish_types
+);
+
+create type ingredient_category as enum ('VEGETABLE', 'ANIMAL', 'MARINE', 'DAIRY', 'OTHER');
+
+create table if not exists ingredient(
+    id serial primary key,
+    name varchar not null,
+    price numeric(10,2),
+    category ingredient_category,
+    id_dish int
+    constraint ingredient_dish_fk
+        foreign key (id_dish)
+        references dish(id)
+)
